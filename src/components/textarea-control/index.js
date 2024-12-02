a7fc5a9cf1eb2f30f03464e2253a27c927de0e90
Abul Kalam Azad <me.azad99@gmail.com>
2024-12-02 21:58:21 +0600

    chore: style add for text area controls

--- src/components/textarea-control/index.js
+++ src/components/textarea-control/index.js
@@ -3,7 +3,7 @@ import { TextareaControl } from '@wordpress/components';
 const BoilerplateTextareaControl = ({ label = 'Textarea', value, onChange, ...props }) => {
 
     return (
-        <div className="boilerplate-blocks-control boilerplate-blocks-textarea-control ">
+        <div className="boilerplate-blocks-control boilerplate-blocks-textarea-control">
             <TextareaControl
                 __nextHasNoMarginBottom
                 label={ label }
