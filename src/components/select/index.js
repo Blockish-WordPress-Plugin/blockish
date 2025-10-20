281f5bdcde91fdcf3e607d68af4b1a8f261e8d61
Mijanur Rahman <mahmudmizan717@gmail.com>
2025-10-20 22:21:58 +0600

    added font family

--- src/components/select/index.js
+++ src/components/select/index.js
@@ -1,11 +1,11 @@
 import Select from "react-select";
-import { BaseControl } from '@wordpress/components';
+import AsyncSelect from "react-select/async";
+import { BaseControl } from "@wordpress/components";
 
-const BlockishSelect = ({ label = 'Select', value, onChange, options=[], ...props }) => {
-
-    return (
-        <div className="blockish-control blockish-select-control">
-			<BaseControl.VisualLabel as="legend" children={label} __nextHasNoMarginBottom={true}/>
+const BlockishSelect = ({ label = 'Select', value, onChange, options = [], ...props }) => {
+	return (
+		<div className="blockish-control blockish-select-control">
+			<BaseControl.VisualLabel as="legend" children={label} __nextHasNoMarginBottom={true} />
 			<Select
 				className="blockish-select"
 				classNamePrefix="blockish-select"
@@ -22,12 +22,42 @@ const BlockishSelect = ({ label = 'Select', value, onChange, options=[], ...prop
 						primary: '#3BAAFE10',
 					},
 				})}
-				{
-					...props
-				}
+				{...props}
 			/>
-        </div>
-    );
-}
+		</div>
+	);
+};
+
+// ✅ Async version
+const BlockishAsyncSelect = ({ label = 'Select', value, onChange, loadOptions, ...props }) => {
+	return (
+		<div className="blockish-control blockish-select-control">
+			<BaseControl.VisualLabel as="legend" children={label} __nextHasNoMarginBottom={true} />
+			<AsyncSelect
+				className="blockish-select"
+				classNamePrefix="blockish-select"
+				value={value}
+				onChange={onChange}
+				loadOptions={loadOptions}
+				defaultOptions
+				cacheOptions
+				isSearchable
+				isClearable
+				theme={(theme) => ({
+					...theme,
+					colors: {
+						...theme.colors,
+						primary25: '#F2F4F7',
+						primary: '#3BAAFE10',
+					},
+				})}
+				{...props}
+			/>
+		</div>
+	);
+};
+
+// Attach Async as a static property
+BlockishSelect.Async = BlockishAsyncSelect;
 
 export default BlockishSelect;
